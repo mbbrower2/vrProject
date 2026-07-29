cc9bf1298857e2397f7700789fcb7375089db840
Maddie Brower <browermaddie@gmail.com>
2026-07-29 16:29:11 -0400

    Add DRT target to PuzzleGame

--- PuzzleGame/Assets/Scripts/DrtTargetSpawner.cs
+++ PuzzleGame/Assets/Scripts/DrtTargetSpawner.cs
@@ -32,23 +32,21 @@ public class DrtTargetSpawner : MonoBehaviour
         Vector3 spawnPos = GetSpawnPosition();
 
         currentTarget = Instantiate(targetPrefab, spawnPos, Quaternion.identity);
-
         DrtTarget targetScript = currentTarget.GetComponent<DrtTarget>();
   
         if (targetScript != null)
         {
             targetScript.Initialize(Time.time, templateValidator.CurrentLevel.templateName);
         }
+
     }
 
     Vector3 GetSpawnPosition()
     {
         if (spawnPoints != null && spawnPoints.Length > 0)
         {
-            int index = Random.Range(0, spawnPoints.Length);
-            return spawnPoints[index].position;
+            return spawnPoints[0].position;
         }
-
         return new Vector3(Random.Range(-5f, 5f), 0f, Random.Range(-5f, 5f));
     }
 }
